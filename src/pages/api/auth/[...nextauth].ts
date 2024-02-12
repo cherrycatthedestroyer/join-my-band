44fe3d29f99e7b54bbe1b0a36ee2ddf665feaeb0
cherrycatthedestroyer <josh96j@gmail.com>
2024-02-12 09:19:25 +0400

    fixed vercel error 3

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,8 +7,14 @@ export const authOptions = {
   secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      clientId:
+        typeof process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID === "string"
+          ? process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
+          : "",
+      clientSecret:
+        typeof process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET === "string"
+          ? process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET
+          : "",
     }),
   ],
 };
